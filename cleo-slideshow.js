7eb7acebd064da2a05612ef53b1791ec4c9c2132
AnnaNovin <annanovin@outlook.com>
2023-10-22 21:05:11 +0300

    cleo slideshow

---
+++ cleo-slideshow.js
@@ -0,0 +1,15 @@
+const catImage = document.querySelector('.cat-info img');
+const imageSources = [
+    'images/cleo2.jpg',
+];
+
+let currentImageIndex = 0;
+
+function updateCatImage() {
+    catImage.src = imageSources[currentImageIndex];
+    currentImageIndex = (currentImageIndex + 1) % imageSources.length;
+}
+
+catImage.addEventListener('click', updateCatImage);
+
+updateCatImage();
